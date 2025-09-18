3b71ef2627a6f4729a3cabe83bec5016e7167a2d
nobodiis <nobodyprodbusiness@gmail.com>
2025-09-18 17:30:24 +0300

    clerk auth

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import "~/styles/globals.css";
 
 import { type Metadata } from "next";
 import { Geist } from "next/font/google";
+import { ClerkProvider } from "@clerk/nextjs";
 
 export const metadata: Metadata = {
   title: "Create T3 App",
@@ -18,8 +19,13 @@ export default function RootLayout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
   return (
-    <html lang="en" className={`${geist.variable}`}>
-      <body>{children}</body>
-    </html>
+    <ClerkProvider>
+      <html lang="en" className={`${geist.variable}`}>
+        <body>
+          {children}
+          <div id="clerk-captcha" style={{ display: 'none' }} />
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
